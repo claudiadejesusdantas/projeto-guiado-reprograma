6f48ea95df375bbc1c51195ab43ba21df400511e
simaraconceicao <contato@simaraconceicao.com>
2022-11-19 17:07:09 -0300

    insere projeto

---
+++ src/pages/Contato.jsx
@@ -0,0 +1,5 @@
+export function Contato() {
+  return(
+    <h1>Inicio na página contato</h1>
+  )
+}
---
+++ src/pages/Sobre.jsx
@@ -0,0 +1,5 @@
+export function Sobre() {
+  return(
+    <h1>Inicio na página sobre</h1>
+  )
+}
